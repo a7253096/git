51eea9f3eb85795a396c712d67c64c540f086651
a7253096 <49856377+a7253096@users.noreply.github.com>
2019-06-25 19:22:29 +0800

    aa

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: yangyun
+ * @Date: 2019/6/25 18:53
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
